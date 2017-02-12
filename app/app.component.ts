57083aab42a7fb5ee58534b2cc1f18931a8c0df3
javiros <javier_rossetti1972@hotmail.com>
2017-02-12 15:49:55 +0000

    Decoupling heroes from appComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,116 +1,16 @@
-// https://angular.io/docs/ts/latest/tutorial/toh-pt3.html
-// NAMING CONVENTIONS SECTION
-
 import { Component } from '@angular/core';
-import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
-const HEROES: Hero[] = [
-	{ id: 11, name: 'Superman', age: 130 },
-	{ id: 12, name: 'Batman', age: 72 },
-	{ id: 13, name: 'Narco', age: 46 },
-	{ id: 14, name: 'Rubberman', age: 56 },
-	{ id: 15, name: 'Finn', age: 16 },
-	{ id: 16, name: 'Jake', age: 11 },
-	{ id: 17, name: 'Maxwel Smart', age: 49 },
-	{ id: 18, name: 'Sledge Hammer', age: 55 },
-	{ id: 19, name: 'Jaime', age: 32 },
-	{ id: 20, name: 'Birdman', age: 60 }
-];
 
 @Component({
-  selector: 'my-app',
-  template: `
-  	<h1>{{title}}</h1>
-    <div class="container">
-      <div class="heroes-list">
-		  	<h2>My Heroes</h2>
-		  	<ul class="heroes">
-		  	  <li 
-		  	  *ngFor="let hero of heroes" 
-		  	  [class.selected]="hero === selectedHero" 
-		  	  (click)="onSelect(hero)">
-		  	    <!-- each hero goes here -->
-		  	    <span class="badge">{{hero.id}}</span> {{hero.name}}, {{hero.age}}
-		  	  </li>
-		  	</ul>
-		  </div>
-		  <div class="hero-details">
-		  	<my-hero-detail [hero]="selectedHero"></my-hero-detail>
-		  </div>  
-	  </div>
-  	`,
-  	styles: [`
-  		.container {
-  			width: 100%
-  		}
-  		.heroes-list {
-  			position: relative;
-  			float: left;
-  			width: 30%
-  		}
-  		.hero-details {
-  			postition: relative;
-  			float: right;
-  			width: 70%;
-  		}
-  	  .selected {
-  	    background-color: #CFD8DC !important;
-  	    color: white;
-  	  }
-  	  .heroes {
-  	    margin: 0 0 2em 0;
-  	    list-style-type: none;
-  	    padding: 0;
-  	    width: 15em;
-  	  }
-  	  .heroes li {
-  	    cursor: pointer;
-  	    position: relative;
-  	    left: 0;
-  	    background-color: #EEE;
-  	    margin: .5em;
-  	    padding: .3em 0;
-  	    height: 1.6em;
-  	    border-radius: 4px;
-  	  }
-  	  .heroes li.selected:hover {
-  	    background-color: #BBD8DC !important;
-  	    color: white;
-  	  }
-  	  .heroes li:hover {
-  	    color: #607D8B;
-  	    background-color: #DDD;
-  	    left: .1em;
-  	  }
-  	  .heroes .text {
-  	    position: relative;
-  	    top: -3px;
-  	  }
-  	  .heroes .badge {
-  	    display: inline-block;
-  	    font-size: small;
-  	    color: white;
-  	    padding: 0.8em 0.7em 0 0.7em;
-  	    background-color: #607D8B;
-  	    line-height: 1em;
-  	    position: relative;
-  	    left: -1px;
-  	    top: -4px;
-  	    height: 1.8em;
-  	    margin-right: .8em;
-  	    border-radius: 4px 0 0 4px;
-  	  }
-  	`]
-
+	selector: 'my-app',
+	template: `
+		<h1>{{title}}</h1>
+		<my-heroes></my-heroes>
+	`,
+  providers: [HeroService]
 })
 
-export class AppComponent { 
+export class AppComponent {
 	title = 'Tour of Heroes';
-	heroes = HEROES;
-	selectedHero: Hero;
-
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
 }
-
---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Superman', age: 130 },
+	{ id: 12, name: 'Batman', age: 72 },
+	{ id: 13, name: 'Narco', age: 46 },
+	{ id: 14, name: 'Rubberman', age: 56 },
+	{ id: 15, name: 'Finn', age: 16 },
+	{ id: 16, name: 'Jake', age: 11 },
+	{ id: 17, name: 'Maxwel Smart', age: 49 },
+	{ id: 18, name: 'Sledge Hammer', age: 55 },
+	{ id: 19, name: 'Jaime', age: 32 },
+	{ id: 20, name: 'Birdman', age: 60 }
+];
